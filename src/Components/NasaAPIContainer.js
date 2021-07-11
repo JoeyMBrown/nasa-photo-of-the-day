77fb4537a1e95fb2382cfbd4d9a778d6a2f58815
JoeyMBrown <Joey.ma.brown@gmail.com>
2021-07-11 13:13:07 -0400

    Added conditional rendering to NasaImg component.  Checks to see if media is an image or not

--- src/Components/NasaAPIContainer.js
+++ src/Components/NasaAPIContainer.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import NasaImg from "./NasaImg";
 
 export default function NasaAPIContainer(props) {
     return (
         <div>
-            <NasaImg hdurl={props.nasaData.hdurl} title={props.nasaData.title}/>
+            <NasaImg hdurl={props.nasaData.hdurl} title={props.nasaData.title} media_type={props.nasaData.media_type}/>
         </div>
     )
 }
--- src/Components/NasaImg.js
+++ src/Components/NasaImg.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-export default function NasaImg ({hdurl, title}) {
+export default function NasaImg ({hdurl, title, media_type}) {
     return (
-        <img src={hdurl} alt={title + " image from NASA's image of the day API"}></img>
+        <div>
+        {
+            media_type !== "image" ? <img src="https://apod.nasa.gov/apod/image/2107/spotthemoon_westlake_2128.jpg" alt="Find the Moon image from NASA's daily image API (7/11/21)" />  : <img src={hdurl} alt={title + " image from NASA's image of the day API"}></img>
+        }
+        </div>
     )
 }
